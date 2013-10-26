1da352cb6e3d0e90c075a621b4100073d6081c17
irmiller22 <irmiller22@gmail.com>
2013-10-26 16:01:54 -0400

    basic data for octokit api

--- octokit_demo.rb
+++ octokit_demo.rb
@@ -13,4 +13,17 @@ user = Octokit.user 'irmiller22'
 binding.pry
 
 # repos = user.rels[:repos].get.data
+
+# User Data
 # repos[0][:full_name]
+# repos[0][:owner][:id]
+# repos[0][:owner][:type]
+# repos[0][:owner][:gravatar_id]
+
+# Last Push data
+# repos[0][:pushed_at]
+
+# Last Repo Create data
+# repos[0][:created_at]
+
+
